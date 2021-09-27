4aaf52c6e5d0a590a7cc7a2425ceb9bb58dcc768
E-Kuru <evandalleaublop@gmail.com>
2021-09-27 12:08:36 +0200

    Exo done

--- jour1.js
+++ jour1.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+var test = "My name is Evan";
+console.log(test);
+
+var firstName = "Evan";
+console.log(`Nice to meet u ${firstName}`);
+
+var testLength = "I'm very long I guess";
+console.log(testLength.length);
+
+var food = "Croissant is meh";
+console.log(food.replace('meh','really good'));
+
+var basic = "This is cool,";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic, basicUp, basicDown);
+
+var word = "Banana";
+var letters = word.split('');
+console.log(letters);
+
+var age = 19;
+var template = `I'm ${age} years old`;
+console.log(template);
+
+var test = "Bonjour";
+var changed = test.replace('o','a');
+console.log(changed);
